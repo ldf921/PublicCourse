633a78215c75cf2c7b058bea8ec0c0b88f8ec7dc
Dingfung Liu <ldf00000@gmail.com>
2018-03-21 22:30:24 +0800

    Implement Roll Pitch Yaw

--- homework3/rotation/rotation.cc
+++ homework3/rotation/rotation.cc
@@ -1,11 +1,17 @@
 // Copyright @2018 Pony AI Inc. All rights reserved.
 
 #include "homework3/rotation/rotation.h"
+#include <math.h>
+#include <iostream>
 
 namespace homework3 {
 
 Eigen::Vector3d ToRollPitchYaw(Eigen::Matrix3d rotation) {
-  return Eigen::Vector3d(0.0, 0.0, 0.0);
+  double roll, pitch, yaw;
+  pitch = asin(rotation(0,2));
+  yaw = atan2(-rotation(0,1), rotation(0, 0));
+  roll = atan2(-rotation(1,2), rotation(2, 2));
+  return Eigen::Vector3d(yaw, pitch, roll);
 }
 
 Eigen::AngleAxisd ToAngleAxis(Eigen::Matrix3d rotation) {
