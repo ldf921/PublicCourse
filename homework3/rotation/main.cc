800de932e38043b8b6a925247caba9f6ce988aad
Dingfung Liu <ldf00000@gmail.com>
2018-03-28 11:36:09 +0800

    Rotation Raw-Pitch-Yaw

--- homework3/rotation/main.cc
+++ homework3/rotation/main.cc
@@ -14,19 +14,27 @@ Eigen::Matrix3d toRotationMatrix(double yaw, double pitch, double roll)
   rot_3 << 1, 0, 0,
            0, cos(roll), -sin(roll),
            0, sin(roll), cos(roll);
-  return rot_3 * rot_1 * rot_2;
+  return rot_2 * rot_1 * rot_3;
 }
 
 int main() {
-//   Eigen::Matrix3d rotation;
-//   rotation << 0.97517033, -0.0978434, -0.19866933,
-//               0.03695701, 0.95642509, -0.28962948,
-//               0.21835066, 0.27509585, 0.93629336;
-//   rotation = toRotationMatrix(1.8, 3, -3.0);
-//   std::cout << rotation << std::endl;
-//   std::cout << homework3::ToRollPitchYaw(rotation) << std::endl;
-//   Eigen::Vector3d  ypr = homework3::ToRollPitchYaw(rotation);
-//   std::cout << rotation - toRotationMatrix(ypr.x(), ypr.y(), ypr.z()) << std::endl;
-//   std::cout << homework3::ToRollPitchYaw(rotation) << std::endl;
+  // Eigen::Matrix3d rotation;
+  // rotation << 0.97517033, -0.0978434, -0.19866933,
+  //             0.03695701, 0.95642509, -0.28962948,
+  //             0.21835066, 0.27509585, 0.93629336;
+  // rotation << 0.97517033, -0.153792, -0.15934508,
+  //             0.0978434, 0.94470249, -0.31299183,
+  //             0.19866933, 0.28962948, 0.93629336;
+  // rotation = toRotationMatrix(0.1, -0.2, 0.3);
+  // std::cout << rotation << std::endl;
+  // std::cout << homework3::ToRollPitchYaw(rotation) << std::endl;
+  // Eigen::Vector3d  ypr = homework3::ToRollPitchYaw(rotation);
+  // std::cout << rotation - toRotationMatrix(ypr.x(), ypr.y(), ypr.z()) << std::endl;
+  // std::cout << homework3::ToRollPitchYaw(rotation) << std::endl;
+  // Eigen::Matrix3d rotation;
+  // rotation << 0.99934681, -0.03017958, 0.0199023,
+  //             0.02997888, 0.99949734, 0.01032745,
+  //             -0.02020334, -0.00972537, 0.99974821;
+  // homework3::ToAngleAxis(rotation);
   return 0;
 }
--- homework3/rotation/rotation.cc
+++ homework3/rotation/rotation.cc
@@ -3,18 +3,33 @@
 #include "homework3/rotation/rotation.h"
 #include <math.h>
 #include <iostream>
+#include "Eigen/LU"
+#include "Eigen/Eigenvalues"
 
 namespace homework3 {
 
 Eigen::Vector3d ToRollPitchYaw(Eigen::Matrix3d rotation) {
   double roll, pitch, yaw;
-  pitch = asin(rotation(0,2));
-  yaw = atan2(-rotation(0,1), rotation(0, 0));
-  roll = atan2(-rotation(1,2), rotation(2, 2));
+  pitch = -asin(rotation(2,0));
+  yaw = atan2(rotation(1,0), rotation(0, 0));
+  roll = atan2(rotation(2,1), rotation(2, 2));
   return Eigen::Vector3d(yaw, pitch, roll);
 }
 
 Eigen::AngleAxisd ToAngleAxis(Eigen::Matrix3d rotation) {
-  return Eigen::AngleAxisd(0.1, Eigen::Vector3d::UnitX());
+  Eigen::EigenSolver<Eigen::Matrix3d> es(rotation);
+  Eigen::Matrix3d diag(es.pseudoEigenvalueMatrix());
+  double mx = 0;
+  int mi;
+  for(int i = 0; i < 3; i++)
+  if (diag(i, i) > mx) {
+    mx = diag(i, i);
+    mi = i;
+  }
+  int xi = (mi + 1) % 3;
+  int yi = (mi + 2) % 3;
+  double angle = -asin(diag(xi, yi)) * es.pseudoEigenvectors().determinant();
+  Eigen::Vector3d axis(es.pseudoEigenvectors().col(mi));
+  return Eigen::AngleAxisd(angle, axis);
 }
 }  // namespace homework3
